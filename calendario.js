2a4dc06644250a86d99a1360b25fe81a765b39f6
RuiShibukawa <130517643+RuiShibukawa@users.noreply.github.com>
2025-06-21 12:15:10 -0300

    21/06/2025 ainda falta logica do calendario e agenda

--- calendario.js
+++ calendario.js
@@ -1,32 +1,25 @@
+// parâmetros passados pelo url
 const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
 const usuario = urlParams.get('perfil');
-
-if(usuario === 'paciente'){
-
-} else if(usuario === 'psicologo'){
-
-}
-
-const listaAgenda = [2, 8, 11, 16, 17];
-
+const agenda = urlParams.get('agenda');
+// tags que serão manipulados
 const daysContainer = document.querySelector(".days");
 const monthYear = document.getElementById("month-year");
 const prevBtn = document.getElementById("prev");
 const nextBtn = document.getElementById("next");
-
+// variáveis do mês
 let date = new Date();
+const year = date.getFullYear();
+const month = date.getMonth();
+const firstDay = new Date(year, month, 1).getDay();
+const lastDate = new Date(year, month + 1, 0).getDate();
+const today = new Date();
+const isCurrentMonth = today.getFullYear() === year && today.getMonth() === month;
+
+const listaAgenda = procurarDias(usuario, agenda);
 
 function renderCalendar(lista) {
-    const year = date.getFullYear();
-    const month = date.getMonth();
-
-    const firstDay = new Date(year, month, 1).getDay();
-    const lastDate = new Date(year, month + 1, 0).getDate();
-
-    const today = new Date();
-    const isCurrentMonth = today.getFullYear() === year && today.getMonth() === month;
-
     const monthNames = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"];
 
     monthYear.textContent = `${monthNames[month]} ${year}`;
@@ -59,3 +52,23 @@ nextBtn.addEventListener("click", () => {
 });
 
 renderCalendar(listaAgenda);
+
+function procurarDias(perfil, agenda){
+    // perfis: psicologo, paciente
+    let listaDias = [];
+    if(perfil === 'psicologo' && agenda === 'psicologo'){
+        listaDias = [2, 8, 11, 16, 17];
+    } else if (perfil === 'paciente' && agenda === 'psicologo'){
+        listaDias = diasUteis(date.getMonth());
+    } else if (perfil === 'paciente' && agenda === 'paciente'){
+        listaDias = [11];
+    }
+    return listaDias;
+}
+
+function diasUteis(mes){
+    let lista = [];
+    
+
+    return lista;
+}
